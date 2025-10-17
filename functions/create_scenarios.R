f775f8e6edb50551d6f3b2c9095517693283a238
Barbora Němcová <barbora.sobolova@kit.edu>
2025-10-17 17:21:54 +0200

    Issue #12: Examine the estimation with the NegBin-Q DGP (#15)

--- functions/create_scenarios.R
+++ functions/create_scenarios.R
@@ -1,25 +1,33 @@
 #' Create the scenario grid for the simulation
 #'
 #' @description Creates the scenario grid of the simulation with
-#'  \begin{itemize}
-#'    \item 2 weekday effect settings (no effect and with effect)
-#'    \item 2 orders of magnitude (5 and 100)
-#'    \item 2 degrees of dispersion (low and high)
-#'    \item 2 true values of the effective reproduction number R
-#'    \item 1 distribution (NegBin-L)
-#'    \item 1 serial interval distribution
+#'   \begin{itemize}
+#'     \item 2 weekday effect settings (no effect and with effect)
+#'     \item 2 orders of magnitude (5 and 100)
+#'     \item 2 degrees of dispersion (low and high)
+#'     \item 2 true values of the effective reproduction number R
+#'     \item 2 distributions (NegBin-L and NegBin-Q)
+#'     \item 1 serial interval distribution
 #'  \end{itemize}
+#'  The final number of simulation scenarios is 24, instead of expected 32,
+#'  because we don't consider scenarios for the NegBin-Q counts together with
+#'  the weekday effects.
 #' @return a data frame with scenario names and parameter values
 create_scenario_grid <- function() {
   scenario_grid <- expand.grid(
     weekday_effect = c("weekday_no", "weekday_yes"),
-    distribution = "NegBin-L",  # Will likely expand later
+    distribution = c("NegBin-L", "NegBin-Q"),
     magnitude = c("low", "high"),
     dispersion = c("low_disp", "high_disp"),
     R_eff = c(1.5, 2.5),
     KEEP.OUT.ATTRS = FALSE,
     stringsAsFactors = FALSE
-  )
+  ) |>
+    # Remove the scenarios, where the counts are NegBin-Q distributed, with the
+    # weekday effect. We don't focus on this scenario in the paper.
+    dplyr::filter(
+      !(distribution == "NegBin-Q" & weekday_effect == "weekday_yes")
+    )
 
   # Pair the dispersion degree name and value
   dispersion <- data.frame(
@@ -29,6 +37,7 @@ create_scenario_grid <- function() {
 
   magnitude <- data.frame(
     magnitude = c("low", "high"),
+    # Parameters for generating the initial values
     init_magnitude = c(5, 100),
     init_sd = c(2, 10),
     init_seed = c(10L, 100L)
@@ -41,8 +50,9 @@ create_scenario_grid <- function() {
     dplyr::mutate(
       scenario_number = seq_len(dplyr::n()),
       scenario_id = paste(
+        "sc",
         distribution,
-        init_magnitude,
+        stringr::str_pad(init_magnitude, 3, pad = "0"),
         dispersion,
         paste0("R", R_eff),
         weekday_effect,
