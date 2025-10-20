a5ae013a851505a24a810821b072a9a9b7fcf050
Barbora Němcová <barbora.sobolova@kit.edu>
2025-10-20 15:05:19 +0200

    Change the overdispersion parameter value

--- functions/create_scenarios.R
+++ functions/create_scenarios.R
@@ -27,7 +27,7 @@ create_scenario_grid <- function(distribution = c("NegBin-L", "NegBin-Q")) {
     nb_size = if (distribution == "NegBin-L") {
       c(2, 0.2)
     } else if (distribution == "NegBin-Q") {
-      c(8, 0.8)
+      c(5, 0.5)
     }
   )
 
