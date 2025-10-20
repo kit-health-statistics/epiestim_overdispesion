5e609e095e3e0d40ccde89d504cd6d879084d63e
Barbora Němcová <barbora.sobolova@kit.edu>
2025-10-20 11:10:54 +0200

    Issue #16: Use statisc branching for the big scenario blocks

--- _targets.R
+++ _targets.R
@@ -42,162 +42,143 @@ global_params <- list(
   short_window = 7,
   long_window = 14,
   n_sim = 1000L,
-  weekday_effect = c(1.05, 0.24, 1.4, 1.69, 0.17, 1.4, 1.05),
+  weekday = c(1.05, 0.24, 1.4, 1.69, 0.17, 1.4, 1.05),
   base_seed = 9786L
 )
 
 # Size of the final plot
 plot_size <- list(width = 14, height = 14.5)
 
+# Scenario blocks for the static branching:
+# NegBin-L
+# NegBin-L with weekday effects
+# NegBin-Q
+outer_scenarios <- data.frame(
+  distribution = c("NegBin-L", "NegBin-L", "NegBin-Q"),
+  weekday_effect = c("weekday_no", "weekday_yes", "weekday_no")
+) |>
+  dplyr::mutate(
+    scenario_id = paste(distribution, weekday_effect, sep = "_")
+  )
+
 # Define pipeline ==============================================================
 list(
-  tar_target(scenarios, create_scenario_grid()),
-
   # Serial interval, common for all scenarios
   tar_target(
     si,
     with(global_params, discr_si(seq_len(n_init), mean_si, std_si))
   ),
-
-  # Initial values. Sample iid counts using a reasonable data generating
-  # mechanism.
-  tar_target(
-    init,
-    {
-      # Set seed ensuring identical initialization for scenarios with the same
-      # magnitude. Creates redundant copies (nrow(scenarios) instead of 2) but
-      # simplifies pipeline structure with negligible computational cost.
-      set.seed(global_params$base_seed + scenarios$init_seed)
-      pmax(
-        0,
-        round(
-          rnorm(
-            global_params$n_init,
-            scenarios$init_magnitude,
-            scenarios$init_sd
+  # Static branching over 3 scenario blocks defined in `outer_scenarios`
+  tar_map(
+    unlist = FALSE,
+    values = outer_scenarios,
+    names = scenario_id,
+    # Get the data frame with scenario parameters
+    tar_target(scenarios, create_scenario_grid(distribution)),
+    # Initial values. Sample iid counts using a reasonable data generating
+    # mechanism.
+    tar_target(
+      init,
+      {
+        # Set seed ensuring identical initialization for scenarios with the same
+        # magnitude. Creates redundant copies (nrow(scenarios) instead of 2) but
+        # simplifies pipeline structure with negligible computational cost.
+        set.seed(global_params$base_seed + scenarios$init_seed)
+        pmax(
+          0,
+          round(
+            rnorm(
+              global_params$n_init,
+              scenarios$init_magnitude,
+              scenarios$init_sd
+            )
           )
         )
-      )
-    },
-    pattern = map(scenarios),
-    iteration = "list"
-  ),
-
-  # Simulations
-  tar_target(
-    trajectories,
-    with(
-      global_params,
-      generate_trajectories(
-        n_sim = n_sim,
-        init,
-        R_eff = scenarios$R_eff,
-        si = si,
-        lgt = n_init + long_window,
-        model = scenarios$distribution,
-        nb_size = scenarios$nb_size,
-        seed = global_params$base_seed + scenarios$scenario_number,
-        # If the weekday effect is not present, we multiply the simulated mean
-        # value by 1 under the hood, so there is indeed no effect
-        weekday_effect = if (scenarios$weekday_effect == "weekday_yes") {
-          global_params$weekday_effect
-        } else {
-          rep(1, global_params$n_init + global_params$long_window)
-        }
-      )
+      },
+      pattern = map(scenarios),
+      iteration = "list"
     ),
-    pattern = map(init, scenarios),
-    iteration = "list"
-  ),
-
-  # Estimation
-  tar_target(
-    df_R_hat,
-    create_results_df(
-      trajectories$X,
-      trajectories$Lambda,
-      global_params$short_window,
-      global_params$long_window
+    # Simulations
+    tar_target(
+      trajectories,
+      with(
+        global_params,
+        generate_trajectories(
+          n_sim = n_sim,
+          init,
+          R_eff = scenarios$R_eff,
+          si = si,
+          lgt = n_init + long_window,
+          model = distribution,
+          nb_size = scenarios$nb_size,
+          seed = global_params$base_seed + scenarios$scenario_number,
+          # If the weekday effect is not present, we multiply the simulated mean
+          # value by 1 under the hood, so there is indeed no effect
+          weekday_effect = if (weekday_effect == "weekday_yes") {
+            global_params$weekday
+          } else {
+            rep(1, global_params$n_init + global_params$long_window)
+          }
+        )
+      ),
+      pattern = map(init, scenarios),
+      iteration = "list"
     ),
-    pattern = map(trajectories),
-    iteration = "list"
-  ),
-
-  # Plot the trajectories and the coverage
-  tar_target(
-    plot_panels,
-    c(
-      trajectories = plot_trajectories(
+    # Estimation
+    tar_target(
+      df_R_hat,
+      create_results_df(
         trajectories$X,
+        trajectories$Lambda,
         global_params$short_window,
-        global_params$n_init
+        global_params$long_window
       ),
-      plot_coverage(
-        scenarios$R_eff,
-        scenarios$nb_size,
-        df_R_hat,
-        trajectories$X[-seq_len(global_params$n_init), ],
-        trajectories$Lambda[-seq_len(global_params$n_init), ],
-        seq(0, 1, by = 0.05),
-        scenarios$distribution,
-        model_colors
-      ),
-      meta = plot_metadata(
-        scenarios$R_eff,
-        scenarios$nb_size,
-        scenarios$magnitude,
-        scenarios$distribution
-      )
+      pattern = map(trajectories),
+      iteration = "list"
     ),
-    pattern = map(trajectories, df_R_hat, scenarios),
-    iteration = "list"
-  ),
-
-  # Save plots
-  tar_target(saved_figures, {
-    # Save only the main simulation without the weekday effects
-    p_simulation <- compose_patches(
-      plot_panels[
-        scenarios$weekday_effect == "weekday_no" &
-          scenarios$distribution == "NegBin-L"
-      ],
-      global_params$short_window,
-      global_params$long_window
-    )
-    save_plot(
-      p_simulation,
-      "simulation_coverage",
-      width = plot_size$width,
-      height = plot_size$height
-    )
-    # Save the supplementary simulation with the weekday effects. The analysis
-    # with the weekday effects was run only for the NegBin-L distribution.
-    p_simulation_weekday <- compose_patches(
-      plot_panels[scenarios$weekday_effect == "weekday_yes"],
-      global_params$short_window,
-      global_params$long_window
-    )
-    save_plot(
-      p_simulation_weekday,
-      "simulation_coverage_weekday_effect",
-      width = plot_size$width,
-      height = plot_size$height
-    )
-
-    # Save the supplementary simulation with NegBin-Q counts
-    p_simulation_nbq <- compose_patches(
-      plot_panels[
-        scenarios$weekday_effect == "weekday_no" &
-          scenarios$distribution == "NegBin-Q"
-      ],
-      global_params$short_window,
-      global_params$long_window
-    )
-    save_plot(
-      p_simulation_nbq,
-      "simulation_coverage_nbq",
-      width = plot_size$width,
-      height = plot_size$height
-    )
-  })
+    # Plot the trajectories and the coverage
+    tar_target(
+      plot_panels,
+      c(
+        trajectories = plot_trajectories(
+          trajectories$X,
+          global_params$short_window,
+          global_params$n_init
+        ),
+        plot_coverage(
+          scenarios$R_eff,
+          scenarios$nb_size,
+          df_R_hat,
+          trajectories$X[-seq_len(global_params$n_init), ],
+          trajectories$Lambda[-seq_len(global_params$n_init), ],
+          seq(0, 1, by = 0.05),
+          distribution,
+          model_colors
+        ),
+        meta = plot_metadata(
+          scenarios$R_eff,
+          scenarios$nb_size,
+          scenarios$magnitude,
+          distribution
+        )
+      ),
+      pattern = map(trajectories, df_R_hat, scenarios),
+      iteration = "list"
+    ),
+    # Save plots
+    tar_target(saved_figures, {
+      # Save the coverage plot
+      p_simulation <- compose_patches(
+        plot_panels,
+        global_params$short_window,
+        global_params$long_window
+      )
+      save_plot(
+        p_simulation,
+        paste(scenario_id, "simulation_coverage", sep = "_"),
+        width = plot_size$width,
+        height = plot_size$height
+      )
+    })
+  )
 )
--- functions/create_scenarios.R
+++ functions/create_scenarios.R
@@ -2,37 +2,30 @@
 #'
 #' @description Creates the scenario grid of the simulation with
 #'   \begin{itemize}
-#'     \item 2 weekday effect settings (no effect and with effect)
 #'     \item 2 orders of magnitude (5 and 100)
 #'     \item 2 degrees of dispersion (low and high)
 #'     \item 2 true values of the effective reproduction number R
-#'     \item 2 distributions (NegBin-L and NegBin-Q)
 #'     \item 1 serial interval distribution
 #'  \end{itemize}
-#'  The final number of simulation scenarios is 24, instead of expected 32,
-#'  because we don't consider scenarios for the NegBin-Q counts together with
-#'  the weekday effects.
+#'  The final number of simulation scenarios is 8.
 #' @return a data frame with scenario names and parameter values
-create_scenario_grid <- function() {
+create_scenario_grid <- function(distribution) {
   scenario_grid <- expand.grid(
-    weekday_effect = c("weekday_no", "weekday_yes"),
-    distribution = c("NegBin-L", "NegBin-Q"),
     magnitude = c("low", "high"),
     dispersion = c("low_disp", "high_disp"),
     R_eff = c(1.5, 2.5),
     KEEP.OUT.ATTRS = FALSE,
     stringsAsFactors = FALSE
-  ) |>
-    # Remove the scenarios, where the counts are NegBin-Q distributed, with the
-    # weekday effect. We don't focus on this scenario in the paper.
-    dplyr::filter(
-      !(distribution == "NegBin-Q" & weekday_effect == "weekday_yes")
-    )
+  )
 
   # Pair the dispersion degree name and value
   dispersion <- data.frame(
     dispersion = c("low_disp", "high_disp"),
-    nb_size = c(2, 0.2)
+    nb_size = if (distribution == "NegBin-L") {
+      c(2, 0.2)
+    } else if (distribution == "NegBin-Q") {
+      c(4, 0.4)
+    }
   )
 
   magnitude <- data.frame(
@@ -51,11 +44,9 @@ create_scenario_grid <- function() {
       scenario_number = seq_len(dplyr::n()),
       scenario_id = paste(
         "sc",
-        distribution,
         stringr::str_pad(init_magnitude, 3, pad = "0"),
         dispersion,
         paste0("R", R_eff),
-        weekday_effect,
         sep = "_"
       )
     )
