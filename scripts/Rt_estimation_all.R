d60576932887b13d0723167a3f0f1babf01a1d82
Barbora Němcová <barbora.sobolova@kit.edu>
2026-03-05 11:31:47 +0100

    Adjust the case study figure

--- scripts/Rt_estimation_all.R
+++ scripts/Rt_estimation_all.R
@@ -11,7 +11,7 @@ source("scripts/analyse_Rt.R")
 # - Ebola in Guinea
 
 # Set the locale to English
-Sys.setlocale("LC_ALL","English")
+Sys.setlocale("LC_ALL", "English")
 
 # List for storing the parameters for the 3 datasets
 params <- replicate(3, list())
@@ -20,7 +20,7 @@ names(params) <- c("flu", "covid", "ebola")
 # Read the flu data --------------------------------------------------
 
 # Data are from:
-# https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1011439#pcbi.1011439.s001
+# https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1011439#pcbi.1011439.s001  # nolint
 incidence_flu <- read_csv("data/flu/daily_flu.csv")
 
 # NOTES:
@@ -39,16 +39,16 @@ params$flu$std_si <- 1.6
 # Read the covid data --------------------------------------------------
 
 # Data from the ECDC web:
-# https://www.ecdc.europa.eu/en/publications-data/data-daily-new-cases-covid-19-eueea-country
-incidence_covid <- read_csv("data/covid/covid_ecdc.csv") %>%
-  filter(geoId == "AT") %>%
-  dplyr::select(dateRep, cases) %>%
-  rename("Date" = "dateRep", "Cases" = "cases") %>%
+# https://www.ecdc.europa.eu/en/publications-data/data-daily-new-cases-covid-19-eueea-country  # nolint
+incidence_covid <- read_csv("data/covid/covid_ecdc.csv") |>
+  filter(geoId == "AT") |>
+  dplyr::select(dateRep, cases) |>
+  rename("Date" = "dateRep", "Cases" = "cases") |>
   mutate(
     Date = as.Date(Date, format = "%d/%m/%Y")
   )
 # Order the data chronologically
-incidence_covid <- incidence_covid[nrow(incidence_covid):1, ]
+incidence_covid <- incidence_covid[rev(seq_len(nrow(incidence_covid))), ]
 
 # We run the analysis on a subset just to keep it shorter
 params$covid$incidence <- incidence_covid
@@ -63,7 +63,7 @@ params$covid$std_si <- 1.83
 
 # Data are from:
 # https://github.com/fintzij/stemr/tree/master/data
-incidence_ebola_daily <-  read_csv(file = "data/ebola/green2022_data_frame.csv")
+incidence_ebola_daily <- read_csv(file = "data/ebola/green2022_data_frame.csv")
 
 # Rescale the parameters to the weekly incidence
 # Parameters of SI distribution taken from Green et al 2022
@@ -79,12 +79,12 @@ scaled_mean <- alpha / scaled_beta
 scaled_sd <- sqrt(alpha / (scaled_beta^2))
 
 # Aggregate the data into weekly format
-incidence_ebola_weekly <- incidence_ebola_daily %>%
-  group_by(week = lubridate::floor_date(date, "week")) %>%
-  summarise(Cases = sum(count)) %>%
-  ungroup() %>%
-  mutate(week = as.Date(week)) %>%
-  filter(week > "2014-02-23") %>%
+incidence_ebola_weekly <- incidence_ebola_daily |>
+  group_by(week = lubridate::floor_date(date, "week")) |>
+  summarise(Cases = sum(count)) |>
+  ungroup() |>
+  mutate(week = as.Date(week)) |>
+  filter(week > "2014-02-23") |>
   mutate(Date = week)
 
 params$ebola$incidence <- incidence_ebola_weekly
@@ -100,46 +100,81 @@ results <- vector("list", 3)
 names(results) <- names(params)
 for (disease in names(results)) {
   results[[disease]] <- with(
-    params[[disease]], 
+    params[[disease]],
     analyse_Rt(incidence, start_date, end_date, window_width, mean_si, std_si)
   )
 }
 
 # Plot the results --------------------------------------------------
 
-# For Ebola, we have trouble fitting the NegBin-L model for 
-# 19. and 26. April 2015. In these 2 estimation windows, the counts are 
+# For Ebola, we have trouble fitting the NegBin-L model for
+# 19. and 26. April 2015. In these 2 estimation windows, the counts are
 # underdispersed. This can be seen also in the Poisson vs. quasi-poisson plot,
-# where the dispersion parameter is estimated as < 1 and the resulting 
+# where the dispersion parameter is estimated as < 1 and the resulting
 # confidence intervals are actually higher for Poisson.
-# As a result, NegBin-L is trying to estimate its dispersion parameter as very 
-# low and runs into the problem of numerical instabilities. For this reason we 
+# As a result, NegBin-L is trying to estimate its dispersion parameter as very
+# low and runs into the problem of numerical instabilities. For this reason we
 # replace these 2 NegBin-L estimates by the Poisson estimates.
-# The changes are carried out in the plot only! If we want to report other 
+# The changes are carried out in the plot only! If we want to report other
 # quantities, we would have to change them too.
 dates_underdisp <- as.Date(c("2015-04-26", "2015-04-19"))
-df_replace <- results$ebola$plt$p_nbin_L_vs_nbin_Q$data |> filter(
-  Date %in% dates_underdisp & Model == "Poiss"
-) |> mutate(Model = "NegBin-L")
+df_replace <- results$ebola$plt$p_nbin_L_vs_nbin_Q$data |>
+  filter(
+    Date %in% dates_underdisp & Model == "Poiss"
+  ) |>
+  mutate(Model = "NegBin-L")
 df_updated <- rows_update(
-  results$ebola$plt$p_nbin_L_vs_nbin_Q$data, 
-  df_replace, 
+  results$ebola$plt$p_nbin_L_vs_nbin_Q$data,
+  df_replace,
   by = c("Date", "Model")
 )
-results$ebola$plt$p_nbin_L_vs_nbin_Q <- results$ebola$plt$p_nbin_L_vs_nbin_Q %+% df_updated
-df_replace <- results$ebola$plt$p_nbin_L_vs_qpois$data |> filter(
-  Date %in% dates_underdisp & Model == "Poiss"
-) |> mutate(Model = "NegBin-L")
+results$ebola$plt$p_nbin_L_vs_nbin_Q <- results$ebola$plt$p_nbin_L_vs_nbin_Q %+%
+  df_updated
+df_replace <- results$ebola$plt$p_nbin_L_vs_qpois$data |>
+  filter(
+    Date %in% dates_underdisp & Model == "Poiss"
+  ) |>
+  mutate(Model = "NegBin-L")
 df_updated <- rows_update(
-  results$ebola$plt$p_nbin_L_vs_qpois$data, 
-  df_replace, 
+  results$ebola$plt$p_nbin_L_vs_qpois$data,
+  df_replace,
   by = c("Date", "Model")
 )
-results$ebola$plt$p_nbin_L_vs_qpois <- results$ebola$plt$p_nbin_L_vs_qpois %+% df_updated
+results$ebola$plt$p_nbin_L_vs_qpois <- results$ebola$plt$p_nbin_L_vs_qpois %+%
+  df_updated
 
-# Adjust individual plots
-plots_to_adjust_date <- c("p_incidence", "p_nbin_L_vs_nbin_Q", "p_pois_vs_qpois",
-                          "p_disp", "p_nbin_L_vs_qpois", "p_nbin_Q_exact_vs_approx")
+# Adjust individual plots *********************************************
+
+# Rescale the y-axis for the Poisson and quasi-Poisson models in the COVID-19
+# examples
+plots_to_adjust_y <- c(
+  "p_nbin_L_vs_nbin_Q",
+  "p_pois",
+  "p_qpois",
+  "p_nbin_L_vs_qpois",
+  "p_nbin_Q_exact_vs_approx"
+)
+for (plt in plots_to_adjust_y) {
+  results$covid$plt[[plt]] <- results$covid$plt[[plt]] +
+    coord_cartesian(
+      ylim = c(0.6, 1.8),
+      xlim = c(
+        min(results$covid$plt[[plt]]$data$Date) - params$covid$window_width,
+        max(results$covid$plt[[plt]]$data$Date)
+      )
+    )
+}
+
+# Add x-axis breaks, which are different for each pathogen
+plots_to_adjust_date <- c(
+  "p_incidence",
+  "p_nbin_L_vs_nbin_Q",
+  "p_pois",
+  "p_qpois",
+  "p_disp",
+  "p_nbin_L_vs_qpois",
+  "p_nbin_Q_exact_vs_approx"
+)
 for (plt in plots_to_adjust_date) {
   results$flu$plt[[plt]] <- results$flu$plt[[plt]] +
     scale_x_date(date_breaks = "2 weeks", date_labels = "%b %d")
@@ -152,7 +187,7 @@ for (plt in plots_to_adjust_date) {
   results$ebola$plt[[plt]] <- results$ebola$plt[[plt]] +
     scale_x_date(date_breaks = "6 months", date_labels = "%b %Y")
 }
-  
+
 # Prepare the annotations
 annotations <- list()
 annotations$flu <- plot_annotation(
@@ -187,69 +222,83 @@ annotations$ebola <- plot_annotation(
       hjust = 0.5,
       face = "bold",
       lineheight = 0.9,
-      margin = margin(l = 51)  # Hardcoded value for centering the plot title
+      margin = margin(l = 51) # Hardcoded value for centering the plot title
     )
   )
 )
 
 # Compose the plots
-p_legend <- wrap_elements(ggpubr::get_legend(results$flu$plt$p_nbin_L_vs_nbin_Q))
+p_legend <- wrap_elements(ggpubr::get_legend(
+  results$flu$plt$p_nbin_L_vs_nbin_Q
+))
 composite_plot_elements <- vector("list", 3)
 names(composite_plot_elements) <- names(params)
 for (disease in names(composite_plot_elements)) {
-  composite_plot_elements[[disease]] <- (
-    with(
-      results[[disease]]$plt,
-      p_incidence / p_pois_vs_qpois / p_nbin_L_vs_nbin_Q
-    ) +
-      plot_layout(heights = c(1, 1, 1), guides = "collect") &
-      theme(legend.position = "none") & 
-      annotations[[disease]]
-  ) |>
+  composite_plot_elements[[disease]] <- (with(
+    results[[disease]]$plt,
+    p_incidence / p_pois / p_qpois / p_nbin_L_vs_nbin_Q
+  ) +
+    plot_layout(heights = c(1, 1, 1, 1), guides = "collect") &
+    theme(legend.position = "none") &
+    annotations[[disease]]) |>
     wrap_elements()
 }
 
 composite_plot <- (
   with(
-    composite_plot_elements, 
+    composite_plot_elements,
     flu | covid | ebola | p_legend
   )
 ) +
   plot_layout(widths = c(3, 3, 3, 1))
-ggsave("figure/composite_plot.pdf", composite_plot, width = 14, height = 11)
-ggsave("figure/composite_plot.png", composite_plot, width = 14, height = 11, dpi = 400)
+ggsave("figure/composite_plot.pdf", composite_plot, width = 14, height = 14)
+ggsave(
+  "figure/composite_plot.png",
+  composite_plot,
+  width = 14,
+  height = 14,
+  dpi = 400
+)
 
 # Supplementary plots --------------------------------------------------
 
 # NegBin-L vs. quasi-Poisson
-p_nbin_L_legend <- wrap_elements(ggpubr::get_legend(results$flu$plt$p_nbin_L_vs_qpois))
+p_nbin_L_legend <- wrap_elements(
+  ggpubr::get_legend(results$flu$plt$p_nbin_L_vs_qpois)
+)
 nbin_L_plot_elements <- vector("list", 3)
 names(nbin_L_plot_elements) <- names(params)
 for (disease in names(nbin_L_plot_elements)) {
-  nbin_L_plot_elements[[disease]] <- (
-    with(
-      results[[disease]]$plt,
-      p_incidence / p_nbin_L_vs_qpois
-    ) +
-      plot_layout(heights = c(1, 1), guides = "collect") &
-      theme(legend.position = "none") & 
-      annotations[[disease]]
-  ) |>
+  nbin_L_plot_elements[[disease]] <- (with(
+    results[[disease]]$plt,
+    p_incidence / p_nbin_L_vs_qpois
+  ) +
+    plot_layout(heights = c(1, 1), guides = "collect") &
+    theme(legend.position = "none") &
+    annotations[[disease]]) |>
     wrap_elements()
 }
 
 nbin_L_plot <- (
   with(
-    nbin_L_plot_elements, 
+    nbin_L_plot_elements,
     flu | covid | ebola | p_nbin_L_legend
   )
 ) +
   plot_layout(widths = c(3, 3, 3, 1))
-ggsave("figure/nbin_L_vs_qpis_plot.pdf", nbin_L_plot, width = 14, height = 6.5)
-ggsave("figure/nbin_L_vs_qpis_plot.png", nbin_L_plot, width = 14, height = 6.5, dpi = 400)
+ggsave("figure/nbin_L_vs_qpois_plot.pdf", nbin_L_plot, width = 14, height = 6.5)
+ggsave(
+  "figure/nbin_L_vs_qpois_plot.png",
+  nbin_L_plot,
+  width = 14,
+  height = 6.5,
+  dpi = 400
+)
 
 # NegBin-Q "exact" vs. approximate
-p_nbin_Q_legend <- wrap_elements(ggpubr::get_legend(results$flu$plt$p_nbin_Q_exact_vs_approx))
+p_nbin_Q_legend <- wrap_elements(
+  ggpubr::get_legend(results$flu$plt$p_nbin_Q_exact_vs_approx)
+)
 nbin_Q_plot_elements <- vector("list", 3)
 names(nbin_Q_plot_elements) <- names(params)
 for (disease in names(nbin_Q_plot_elements)) {
@@ -259,53 +308,64 @@ for (disease in names(nbin_Q_plot_elements)) {
       p_incidence / p_nbin_Q_exact_vs_approx
     ) +
       plot_layout(heights = c(1, 1), guides = "collect") &
-      theme(legend.position = "none") & 
+      theme(legend.position = "none") &
       annotations[[disease]]
   ) |>
     wrap_elements()
 }
 
 nbin_Q_plot <- (
-  with(
-    nbin_Q_plot_elements, 
-    flu | covid | ebola | p_nbin_Q_legend
-  )
+  with(nbin_Q_plot_elements, flu | covid | ebola | p_nbin_Q_legend)
 ) +
   plot_layout(widths = c(3, 3, 3, 1))
-ggsave("figure/nbin_Q_approximation_plot.pdf", nbin_Q_plot, width = 14, height = 6.5)
-ggsave("figure/nbin_Q_approximation_plot.png", nbin_Q_plot, width = 14, height = 6.5, dpi = 400)
+ggsave(
+  "figure/nbin_Q_approximation_plot.pdf",
+  nbin_Q_plot,
+  width = 14,
+  height = 6.5
+)
+ggsave(
+  "figure/nbin_Q_approximation_plot.png",
+  nbin_Q_plot,
+  width = 14,
+  height = 6.5,
+  dpi = 400
+)
 
 # Overdispersion parameter estimates over time
 p_disp_legend <- wrap_elements(ggpubr::get_legend(results$flu$plt$p_disp))
 disp_plot_elements <- vector("list", 3)
 names(disp_plot_elements) <- names(params)
 for (disease in names(disp_plot_elements)) {
-  # Change the hardcoded margin back to another value to center the plot title. 
+  # Change the hardcoded margin back to another value to center the plot title.
   # It is necessary here, because we have the second y-axis
   annotations[[disease]]$theme$plot.title$margin <- switch(
-    disease, 
+    disease,
     flu = margin(l = 9),
     covid = margin(l = 16),
     ebola = margin(l = 3)
   )
-  disp_plot_elements[[disease]] <- (
-    with(
-      results[[disease]]$plt,
-      p_incidence / p_disp
-    ) +
-      plot_layout(heights = c(1, 1), guides = "collect") &
-      theme(legend.position = "none") & 
-      annotations[[disease]]
-  ) |>
+  disp_plot_elements[[disease]] <- (with(
+    results[[disease]]$plt, p_incidence / p_disp
+  ) +
+    plot_layout(heights = c(1, 1), guides = "collect") &
+    theme(legend.position = "none") &
+    annotations[[disease]]) |>
     wrap_elements()
 }
 
-disp_plot <- (
-  with(
-    disp_plot_elements, 
-    flu | covid | ebola | p_disp_legend
-  )
-) +
+disp_plot <- (with(disp_plot_elements, flu | covid | ebola | p_disp_legend)) +
   plot_layout(widths = c(3, 3, 3, 1))
-ggsave("figure/overdispersion_parameters.pdf", disp_plot, width = 14, height = 6.5)
-ggsave("figure/overdispersion_parameters.png", disp_plot, width = 14, height = 6.5, dpi = 400)
+ggsave(
+  "figure/overdispersion_parameters.pdf",
+  disp_plot,
+  width = 14,
+  height = 6.5
+)
+ggsave(
+  "figure/overdispersion_parameters.png",
+  disp_plot,
+  width = 14,
+  height = 6.5,
+  dpi = 400
+)
